74ba5e5aefec48f91ae5f6fed4bcfe6a1cf1cf67
Khaya Khumalo <khaya.khumalo93@gmail.com>
2024-08-29 21:51:45 +0100

    Updating freelance gigs

---
+++ _site/script.js
@@ -0,0 +1,15 @@
+// window.addEventListener('load', function() {
+//     const divs = document.querySelectorAll('.gig-content');
+//     let maxHeight = 0;
+
+//     divs.forEach(div => {
+//         const divHeight = div.offsetHeight;
+//         if (divHeight > maxHeight) {
+//             maxHeight = divHeight;
+//         }
+//     });
+
+//     divs.forEach(div => {
+//         div.style.height = maxHeight + 'px';
+//     });
+// });
